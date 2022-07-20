8bb541e50d0c00a8a602e9bc9bf2514545abc9bd
jiun <vonreal20@gmail.com>
2022-07-20 09:57:54 +0900

    🌱 [0720 - 수업] - 0: 0719 과제 함께 구현

---
+++ TrendMedia/SearchMovieTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SearchMovieTableViewCell.swift
+//  TrendMedia
+//
+//  Created by 나지운 on 2022/07/20.
+//
+
+import UIKit
+
+class SearchMovieTableViewCell: UITableViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+}
---
+++ TrendMedia/SearchMovieTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SearchMovieTableViewController.swift
+//  TrendMedia
+//
+//  Created by 나지운 on 2022/07/20.
+//
+
+import UIKit
+
+class SearchMovieTableViewController: UITableViewController {
+
+    var movieList = ["셰이프 오브 워터", "판의 미로", "헬보이"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchMovieTableViewCell", for: indexPath) as! SearchMovieTableViewCell
+        
+        cell.titleLabel.text = movieList[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // UIScreen.main.bounds = 디바이스 전체 높이
+        // 몇 개의 셀을 보여줄 것인가
+        return UIScreen.main.bounds.height / 8
+    }
+}
